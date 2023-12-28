92357959455273776cc29de190543ef597163c18
Md Nasim Hosen <mdnasimhosen333@gmail.com>
2023-12-28 21:42:21 +0600

    password strong for special charecter

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -9,7 +9,16 @@ const changePasswordValidationSchema = z.object({
   currentPassword: z.string({
     required_error: "Current password is required",
   }),
-  newPassword: z.string({ required_error: "Password is required" }),
+  newPassword: z
+    .string()
+    .min(8)
+    .refine(
+      (data) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\d])/.test(data),
+      {
+        message:
+          "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+      }
+    ),
 });
 
 export const AuthValidation = {
--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -3,7 +3,16 @@ import { z } from "zod";
 const createUserValidationSchema = z.object({
   username: z.string(),
   email: z.string().email(),
-  password: z.string(),
+  password: z
+    .string()
+    .min(8)
+    .refine(
+      (data) => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z\d])/.test(data),
+      {
+        message:
+          "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+      }
+    ),
   role: z.enum(["user", "admin"]).default("user"),
 });
 
